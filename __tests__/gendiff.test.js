414799e0e60c52da3b4037c0a87dfe073dc316a9
Igor Shayderov <flashmob123@gmail.com>
2022-05-22 19:26:34 +0200

    Gendiff tests

--- __tests__/gendiff.test.js
+++ __tests__/gendiff.test.js
@@ -2,16 +2,52 @@ import { genDiff } from '../src/main';
 
 describe('genDiff function', () => {
   test.each([
-    ['similar property', { _: '_' }, { _: '_' }, '{\n   _: _\n}'],
-    ['existing property', { _: '_' }, {}, '{\n + _: _\n}'],
-    ['non-existing property', {}, { _: '_' }, '{\n - _: _\n}'],
-    ['existing property with different value', { _: '__' }, { _: '_' }, '{\n + _: __\n - _: _\n}'],
+    ['properties are similar',
+      { _: '_' }, { _: '_' }, {
+        _: {
+          isExistsInFirstFile: true,
+          isExistsInSecondFile: true,
+          areValuesEqual: true,
+          firstFileValue: '_',
+          secondFileValue: '_',
+        },
+      }],
+    ['propety exists in first file',
+      { _: '_' }, {}, {
+        _: {
+          isExistsInFirstFile: true,
+          isExistsInSecondFile: false,
+          areValuesEqual: false,
+          firstFileValue: '_',
+          secondFileValue: undefined,
+        },
+      }],
+    ['propety exists in second file',
+      {}, { _: '_' }, {
+        _: {
+          isExistsInFirstFile: false,
+          isExistsInSecondFile: true,
+          areValuesEqual: false,
+          firstFileValue: undefined,
+          secondFileValue: '_',
+        },
+      }],
+    ['property exsits in both files, but value is different',
+      { _: '_' }, { _: '__' }, {
+        _: {
+          isExistsInFirstFile: true,
+          isExistsInSecondFile: true,
+          areValuesEqual: false,
+          firstFileValue: '_',
+          secondFileValue: '__',
+        },
+      }],
   ])(
-    'should show %s',
+    'should match when %s',
     (description, firstFile, secondFile, expected) => {
       const result = genDiff(firstFile, secondFile);
 
-      expect(result).toBe(expected);
+      expect(result).toStrictEqual(expected);
     },
   );
 });
